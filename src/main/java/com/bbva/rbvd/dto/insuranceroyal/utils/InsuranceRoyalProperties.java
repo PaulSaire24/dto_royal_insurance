b54bb1d396593e674f661abd92fcaf2f0d77fc9b
AUGUSTO EDUARDO MARTINEZ SALAZAR <P029820@PE.igrupobbva>
2022-11-15 17:21:43 -0500

    fix:adding uri for simulation

--- src/main/java/com/bbva/rbvd/dto/insuranceroyal/utils/InsuranceRoyalProperties.java
+++ src/main/java/com/bbva/rbvd/dto/insuranceroyal/utils/InsuranceRoyalProperties.java
@@ -6,6 +6,8 @@ public enum InsuranceRoyalProperties {
 	FIELD_INSURANCE_PRODUCT_DESC("INSURANCE_PRODUCT_DESC"),
 	FIELD_INSURANCE_BUSINESS_NAME("INSURANCE_BUSINESS_NAME"),
 	QUERY_GET_PRODUCT_INFORMATION("PISD.GET_PRODUCT_INFORMATION"),
+	URI_SIMULATION_PT_KEY("rimac.simulation.pt.uri"),
+	ID_API_PT_INSURANCE_SIMULATION_RIMAC("api.connector.simulationPT.rimac.url"),
 	URI_QUOTATION_PT_KEY("rimac.quotation.pt.uri"),
 	
 	ID_API_PT_INSURANCE_QUOTATION_RIMAC("api.connector.quotationPT.rimac.url");
