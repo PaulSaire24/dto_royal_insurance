fca6b8a7b4f22ee31731272082764e0817e7d2b8
Fredy Cerron <fredy.cerron.contractor@bbva.com>
2023-09-15 08:03:34 -0500

    feat: PERAR-24655 PERAR-24935 refundcalculate package added

---
+++ src/main/java/com/bbva/rbvd/dto/insuranceroyal/rimac/pt/refundCalculate/RefundCalculatedBO.java
@@ -0,0 +1,20 @@
+package com.bbva.rbvd.dto.insuranceroyal.rimac.pt.refundCalculate;
+
+public class RefundCalculatedBO {
+    private RefundCalculatedPayloadBO payload;
+
+    public RefundCalculatedPayloadBO getPayload() {
+        return payload;
+    }
+
+    public void setPayload(RefundCalculatedPayloadBO payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public String toString() {
+        return "RefundCalculatedBO{" +
+                "payload=" + payload +
+                '}';
+    }
+}
