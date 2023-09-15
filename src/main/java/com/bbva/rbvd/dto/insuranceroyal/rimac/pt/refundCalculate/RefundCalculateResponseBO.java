f568a47f73235d251a2a3862eae75540b03b5686
Fredy Cerron <fredy.cerron.contractor@bbva.com>
2023-09-15 18:24:30 -0500

    feat: PERAR-24655 PERAR-24935 response class created

---
+++ src/main/java/com/bbva/rbvd/dto/insuranceroyal/rimac/pt/refundCalculate/RefundCalculateResponseBO.java
@@ -0,0 +1,32 @@
+package com.bbva.rbvd.dto.insuranceroyal.rimac.pt.refundCalculate;
+
+import java.util.List;
+
+public class RefundCalculateResponseBO {
+    private Integer plazo;
+    private List<Integer> porcentaje;
+
+    public Integer getPlazo() {
+        return plazo;
+    }
+
+    public void setPlazo(Integer plazo) {
+        this.plazo = plazo;
+    }
+
+    public List<Integer> getPorcentaje() {
+        return porcentaje;
+    }
+
+    public void setPorcentaje(List<Integer> porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    @Override
+    public String toString() {
+        return "RefundCalculateResponseBO{" +
+                "plazo=" + plazo +
+                ", porcentaje=" + porcentaje +
+                '}';
+    }
+}
