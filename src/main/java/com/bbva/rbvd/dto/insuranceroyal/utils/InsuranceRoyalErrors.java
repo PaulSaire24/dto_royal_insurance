f660e0ff4d605ddc068cebcda5feeb4197254254
paulkyev <paul.saire.contractor@bbva.com>
2023-11-20 13:01:22 -0500

    feat PERAR-25613 PERAR-25656 add error tier ASO

--- src/main/java/com/bbva/rbvd/dto/insuranceroyal/utils/InsuranceRoyalErrors.java
+++ src/main/java/com/bbva/rbvd/dto/insuranceroyal/utils/InsuranceRoyalErrors.java
@@ -6,7 +6,8 @@ public enum InsuranceRoyalErrors {
     SERVER_ERROR("RBVD00000158", false, "Ocurrio un problema en el servidor"),
     CLIENT_WITHOUT_PT_CONTRACT("RBVD00120018", false, "El cliente no cuenta con un contrato PT"),
     INSERTION_ERROR_IN_SIMULATION_TABLE("RBVD00120021", false, "Parametros requeridos incompletos para el registro de simulacion"),
-    ERROR_FROM_RIMAC("RBVD00000119", false, "Error desde Rimac con la cotizacion enviada");
+    ERROR_FROM_RIMAC("RBVD00000119", false, "Error desde Rimac con la cotizacion enviada"),
+    ERROR_CONNECTION_TIER_ASO_SERVICE("RBVD00120003", false, "No se pudo realizar una conexión con el servicio de Clasificación de Cliente");
     private final String adviceCode;
     private final boolean rollback;
     private final String message;
